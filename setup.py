e1db4c7623cdebb833483c70fefdb9c71a4e6ff1
Sambodhi Bhattacharyya <sambodhi.b@gmail.com>
2020-04-11 22:52:39 +0530

    Modified Setup file and added requirements file for dev and prod installs

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
 from setuptools import setup, find_packages
 
+requirements = [
+    'numpy',
+]
+
+dev_requirements = [
+    'wheel',
+    'pytest',
+]
+
 setup(
     name='ScMatSort',
     version='0.1',
+    author='Sambodhi Bhattacharyya',
+    author_email='sambodhi.b@gmail.com',
     packages=find_packages('src'),
-    package_dir={"": "src"},
+    package_dir={"": "src",},
+    install_requires=requirements,
+    extras_require={
+        'dev': dev_requirements,
+        'jedi': ['jedi'],
+    },
 )
